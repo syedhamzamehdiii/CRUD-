4bca300c2f965b58e8ef92ca822bdee75d6c4078
syedhamzamehdiii <mehihamza1@gmail.com>
2024-07-07 19:14:35 +0500

    Create User.js
    Database

---
+++ User.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  customId: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
